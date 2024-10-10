2f828de91c3498ebd95af4f7e05d3df7e9cc9844
sooho72 <rhkdgks72@naver.com>
2024-10-10 09:15:22 +0900

    DB연결 불러오기

--- src/test/java/com/pgm/springdemo/sample/SampleTest.java
+++ src/test/java/com/pgm/springdemo/sample/SampleTest.java
@@ -1,10 +1,7 @@
 package com.pgm.springdemo.sample;
 
 import com.pgm.springdemo.config.AppConfig;
-
-
-import lombok.extern.log4j.Log4j;
-
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,35 +13,60 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+
 @Log4j2
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations={"/test-context.xml"})
+@ExtendWith({SpringExtension.class})
+//@ContextConfiguration(classes = AppConfig.class)
+@ContextConfiguration(locations ={"/test-context.xml","file:src/main/webapp/WEB-INF/root-context.xml"})
+//@RequiredArgsConstructor
 public class SampleTest {
     @Autowired
-    @Qualifier("sample1")
-    private SampleDTO sampleDTO;
+    private DataSource dataSource;
+    @Autowired(required = false)
+    private SampleService sampleService;
+//    @Autowired
+//    @Qualifier("myDto1")
+//    private SampleDTO sampleDTO1;
+
+    @Test
+    public void ConnectionTest() throws Exception {
+        Connection connection = dataSource.getConnection();
+        log.info(connection);
+    }
+
+    @Test
+    public void setSampleServiceTest(){
+        log.info(sampleService);
+    }
+
+    @Test
+    public void testSample1() {
+        //log.info(sampleDTO1);
+    }
 
     @Test
     public void testSample() {
         SampleDTO dto = new SampleDTO();
         dto.setAge(20);
         dto.setName("박경미");
-        String[] hobbys = {"수영", "영화감상"};
+        String[] hobbys={"수영","영화감상"};
         dto.setHobby(hobbys);
         log.info(dto);
     }
 
     @Test
-    public void testSample2() {
+    public void testSaple2(){
         ApplicationContext context = new ClassPathXmlApplicationContext("/test-context.xml");
         SampleDTO dto1 = (SampleDTO) context.getBean("sample1");
         log.info(dto1);
         SampleDTO dto2 = (SampleDTO) context.getBean("sample2");
         log.info(dto2);
     }
-
     @Test
-    public void testSample3() {
+    public void testSaple3(){
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         SampleDTO dto1 = (SampleDTO) context.getBean("myDto1");
         log.info(dto1);
